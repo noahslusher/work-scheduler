6baf28d91e9c0699e64fd3ecf1b01e2964be827f
Carl Wheezer <slushern42@gmail.com>
2022-02-27 12:41:04 -0700

    messing with the input button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,50 @@
 // Declare variables
 var saveButton = document.querySelector('.btn')
-var input = $('input').val()
-console.log(input)
 
 
 // Assign saveTasks function to button
 $('.btn').click(function () {
- saveTasks()
+var input = $('textarea').val()
+ saveTasks(input)
+ console.log(input)
 })
 
 // saveTasks function
-function saveTasks() {
+function saveTasks(input) {
  localStorage.setItem("task", JSON.stringify(input))
 }
+
+// //Change textarea background color based on time
+// var checkTime = function () {
+
+//  //Get Current time
+//  var currentTime = moment().format('H');
+
+//  //get all elements with class "taskarea"
+//  var timeBlockElements = $(".input");
+
+//  //loop through taskarea classes
+//  for (var i = 0 ; i < timeBlockElements.length ; i++) {
+
+//      //Get element i's ID as a string
+//      var elementID = timeBlockElements[i].id;
+
+//      //get element by ID
+//      var manipID = document.getElementById(timeBlockElements[i].id)
+
+//      //remove any old classes from element
+//      $(timeBlockElements[i].id).removeClass(".present .past .future");
+
+//      // apply new class if task is present/past/future
+//      if (elementID < currentTime) {
+//          $(manipID).addClass("past");
+//      } else if (elementID > currentTime) {
+//          $(manipID).addClass("future");
+//      } else {
+//          $(manipID).addClass("present");
+//      }
+//  }
+// }
+
+// // checkTime every 5 minutes
+// setInterval(checkTime(), (1000 * 60) * 5);
